651a85c1f2c0d9a35d6dde8539ec056fc007290c
Martin Elliott <martin@chirohosting.com>
2023-07-09 16:54:05 +0100

    Updated fields.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mooglescreations-api
 API endpoints for Moogle's Creations
+
+Instagram useful links:
+
+https://docs.oceanwp.org/article/487-how-to-get-instagram-access-token
+
+https://blog.devgenius.io/how-add-your-instagram-posts-to-your-react-project-e8527d2a7187
--- instagramGetPosts.js
+++ instagramGetPosts.js
@@ -32,19 +32,20 @@ async function getMediaList() {
    });
 
    for (let i = 0; i < posts.length; i++) {
-      const media_url = 'https://graph.instagram.com/' + posts[i] + '?access_token=' + token + '&fields=media_url,media_type,permalink,caption,timestamp';
+      const media_url = 'https://graph.instagram.com/' + posts[i] + '?access_token=' + token + '&fields=media_url,media_type,permalink,caption,timestamp,thumbnail_url';
       const media_response = await axios.get(media_url);
       const media = [
          media_response.data.id,
          media_response.data.media_url,
          media_response.data.media_type,
+         media_response.data.thumbnail_url,
          media_response.data.caption,
          media_response.data.permalink,
          media_response.data.timestamp
       ];
-      console.log(media);
+      console.log(media_response.data.thumbnail_url);
 
-      connection.query("INSERT INTO instagram_feed VALUES (?) ON DUPLICATE KEY UPDATE media_url=VALUES(media_url), media_type=VALUES(media_type), caption=VALUES(caption), permalink=VALUES(permalink), timestamp=VALUES(timestamp)",
+      connection.query("INSERT INTO instagram_feed VALUES (?) ON DUPLICATE KEY UPDATE media_url=VALUES(media_url), media_type=VALUES(media_type), thumbnail_url=VALUES(thumbnail_url), caption=VALUES(caption), permalink=VALUES(permalink), timestamp=VALUES(timestamp)",
          [media], function (error, result) {
             if (error) throw error;
             console.log('Row inserted successfully!');
